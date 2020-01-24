17eea4ee8493164d9130124d86e0b38b648b9b19
Xiaoqiang Zheng <zhengxq@gmail.com>
2020-01-23 22:35:36 -0800

    Refactor CodeGen from SimpleIREval (#47)

---
+++ torch/csrc/jit/tensorexpr/codegen.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include "torch/csrc/jit/tensorexpr/buffer.h"
+#include "torch/csrc/jit/tensorexpr/ir.h"
+#include "torch/csrc/jit/tensorexpr/tensor.h"
+
+namespace torch {
+namespace jit {
+namespace compiler {
+
+template <typename T>
+class PaddedBuffer;
+
+class CodeGen {
+ public:
+  class BufferArg;
+  class CallArg;
+
+  template <typename... Ts>
+  CodeGen(const Stmt& stmt, Ts... ts)
+      : ir_node_(stmt.node()), buffer_args_({BufferArg(ts)...}) {}
+
+  template <typename... Ts>
+  CodeGen(const Expr& expr, Ts... ts)
+      : ir_node_(expr.node()), buffer_args_({BufferArg(ts)...}) {}
+
+  RefHandle<IRNode>& ir_node() {
+    return ir_node_;
+  }
+
+  const RefHandle<IRNode>& ir_node() const {
+    return ir_node_;
+  }
+
+  std::vector<BufferArg>& buffer_args() {
+    return buffer_args_;
+  }
+
+  const std::vector<BufferArg>& buffer_args() const {
+    return buffer_args_;
+  }
+
+ private:
+  RefHandle<IRNode> ir_node_;
+  std::vector<BufferArg> buffer_args_;
+};
+
+class CodeGen::BufferArg {
+ public:
+  BufferArg(const Buffer& buffer) : var_(buffer.data()) {}
+  BufferArg(const Tensor& tensor) : var_(tensor.function().func_var()) {}
+  BufferArg(const Function& func) : var_(func.func_var()) {}
+  const Var& var() const {
+    return var_;
+  }
+  Var& var() {
+    return var_;
+  }
+
+ private:
+  Var var_;
+};
+
+class CodeGen::CallArg {
+ public:
+  template <typename T>
+  CallArg(const PaddedBuffer<T>& buffer);
+
+  template <typename T>
+  CallArg(const std::vector<T>& buffer) : ptr_(const_cast<T*>(buffer.data())) {}
+
+  void* data() {
+    return ptr_;
+  }
+
+ private:
+  void* ptr_ = nullptr;
+};
+
+} // namespace compiler
+} // namespace jit
+} // namespace torch
