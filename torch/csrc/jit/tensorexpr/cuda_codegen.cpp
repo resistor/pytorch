aa099b8ad4d112c45d6e94ce1d3c6f59b3a638bc
Xiaoqiang Zheng <zhengxq@gmail.com>
2020-01-31 00:37:39 -0800

    Refactor UniqueNameManager into its own files. (#79)

---
+++ torch/csrc/jit/tensorexpr/cuda_codegen.cpp
@@ -0,0 +1,9 @@
+#include "torch/csrc/jit/tensorexpr/cuda_codegen.h"
+
+namespace torch {
+namespace jit {
+namespace tensorexpr {
+
+} // namespace tensorexpr
+} // namespace jit
+} // namespace torch
