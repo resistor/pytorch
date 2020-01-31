869127ce8cda3f2c057d3f243bb9634c7ac1395a
Xiaoqiang Zheng <zhengxq@gmail.com>
2020-01-31 00:37:39 -0800

    Refactor UniqueNameManager into its own files. (#79)

--- torch/csrc/jit/tensorexpr/unique_name_manager.cpp
+++ torch/csrc/jit/tensorexpr/unique_name_manager.cpp
@@ -1,8 +1,5 @@
 #include "torch/csrc/jit/tensorexpr/unique_name_manager.h"
 
-#include <cctype>
-#include "torch/csrc/jit/tensorexpr/ir.h"
-
 namespace torch {
 namespace jit {
 namespace tensorexpr {
@@ -16,24 +13,20 @@ const std::string& UniqueNameManager::get_unique_name(const Variable* v) {
 
   // First use the name_hint as a prefix to check if there is another name
   // with the same prefix.
-  std::string name_hint = v->name_hint();
-  if (name_hint == "") {
-    name_hint = "v";
-  } else if (std::isdigit(name_hint[0])) {
-    name_hint = "v" + name_hint;
-  }
+  const std::string& name_hint = v->name_hint();
   int& count = unique_name_count_[name_hint];
-  while (true) {
+  while (1) {
     // Even if with a new count, this name might already be used. For example
     // ("x", 1) could collidewith ("x_1", 0)
     int count_v = count++;
     std::string unique_name = name_hint;
-    if (count_v > 0) {
+    if (count_v > -1) {
       unique_name += "_" + std::to_string(count_v);
     }
     if (all_unique_names_.count(unique_name) == 0) {
       all_unique_names_.insert(unique_name);
-      auto result = unique_name_mapping_.insert(std::make_pair(v, unique_name));
+      auto result =
+	  unique_name_mapping_.insert(std::make_pair(v, unique_name));
       return result.first->second;
     }
   }
--- torch/csrc/jit/tensorexpr/unique_name_manager.h
+++ torch/csrc/jit/tensorexpr/unique_name_manager.h
@@ -1,28 +1,24 @@
 #pragma once
 
-#include <string>
 #include <unordered_map>
 #include <unordered_set>
 
-#include <torch/csrc/WindowsTorchApiMacro.h>
+#include "torch/csrc/jit/tensorexpr/ir.h"
 
 namespace torch {
 namespace jit {
 namespace tensorexpr {
 
-class Var;
-class Variable;
-
 using VarNameMap = std::unordered_map<const Variable*, std::string>;
 
 // A manager to get unique names from vars.
-// It starts with the name hints of the var and append "_" + $counter until it
-// hits a unique name.
+// It starts with the name hints of the var and append "_" + $counter until it hits a unique
+// name.
 class TORCH_API UniqueNameManager {
  public:
-  const std::string& get_unique_name(const Var& v);
+  TORCH_API const std::string& get_unique_name(const Var& v);
 
-  const std::string& get_unique_name(const Variable* v);
+  TORCH_API const std::string& get_unique_name(const Variable* v);
 
  private:
   friend class ScopedVarName;
@@ -30,7 +26,7 @@ class TORCH_API UniqueNameManager {
   std::unordered_map<std::string, int> unique_name_count_;
   std::unordered_set<std::string> all_unique_names_;
 };
-
+  
 } // namespace tensorexpr
 } // namespace jit
 } // namespace torch
