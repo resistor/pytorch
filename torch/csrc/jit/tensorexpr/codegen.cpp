a309cf4fd7f9576b906a5d55fb47ea3bed5366fd
Xiaoqiang Zheng <zhengxq@gmail.com>
2020-02-06 13:44:41 -0800

    Refactor CudaCodeGen into generic registration, so we can have both the Cuda and non-Cuda builds. (#118)

---
+++ torch/csrc/jit/tensorexpr/codegen.cpp
@@ -0,0 +1,65 @@
+#include "torch/csrc/jit/tensorexpr/codegen.h"
+
+namespace torch {
+namespace jit {
+namespace tensorexpr {
+
+RegisterCodeGenList::StmtFactoryMethod RegisterCodeGenList::
+    FindStmtFactoryMethod(const std::string& name) {
+  auto iter = stmt_factory_methods_.find(name);
+  if (iter == stmt_factory_methods_.end()) {
+    throw std::runtime_error("Invalid codegen name: " + name);
+  }
+  return iter->second;
+}
+
+RegisterCodeGenList::ExprFactoryMethod RegisterCodeGenList::
+    FindExprFactoryMethod(const std::string& name) {
+  auto iter = expr_factory_methods_.find(name);
+  if (iter == expr_factory_methods_.end()) {
+    throw std::runtime_error("Invalid codegen name: " + name);
+  }
+  return iter->second;
+}
+
+void RegisterCodeGenList::AddStmtFactoryMethod(
+    const std::string& name,
+    StmtFactoryMethod stmt_factory_method) {
+  auto insert_ret =
+      stmt_factory_methods_.insert(std::make_pair(name, stmt_factory_method));
+  if (!insert_ret.second) {
+    throw std::runtime_error("Duplicated CodeGen names: " + name);
+  }
+}
+
+void RegisterCodeGenList::AddExprFactoryMethod(
+    const std::string& name,
+    ExprFactoryMethod expr_factory_method) {
+  auto insert_ret =
+      expr_factory_methods_.insert(std::make_pair(name, expr_factory_method));
+  if (!insert_ret.second) {
+    throw std::runtime_error("Duplicated CodeGen names: " + name);
+  }
+}
+
+std::unique_ptr<CodeGen> CreateCodeGen(
+    const std::string& name,
+    const Stmt& stmt,
+    const std::vector<CodeGen::BufferArg>& params) {
+  RegisterCodeGenList::StmtFactoryMethod method =
+      RegisterCodeGenList::GetInstance().FindStmtFactoryMethod(name);
+  return method(stmt, params);
+}
+
+std::unique_ptr<CodeGen> CreateCodeGen(
+    const std::string& name,
+    const Expr& expr,
+    const std::vector<CodeGen::BufferArg>& params) {
+  RegisterCodeGenList::ExprFactoryMethod method =
+      RegisterCodeGenList::GetInstance().FindExprFactoryMethod(name);
+  return method(expr, params);
+}
+
+} // namespace tensorexpr
+} // namespace jit
+} // namespace torch
