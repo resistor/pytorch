b7d3e2e189b42ff2a275bebaa9c2c0fa058150d8
Bert Maher <bertmaher@users.noreply.github.com>
2020-02-21 15:47:12 -0800

    initial impl of symbolic shapes (#176)
    * formatted guard elimination
    
    * initial impl of symbolic shapes

--- aten/src/ATen/core/interned_strings.h
+++ aten/src/ATen/core/interned_strings.h
@@ -99,6 +99,7 @@ namespace c10 {
   _(prim, isinstance)                \
   _(prim, tolist)                    \
   _(prim, unchecked_cast)            \
+  _(prim, inflate)                   \
   _(aten, _grad_sum_to_size)         \
   _(aten, _size_if_not_equal)        \
   _(aten, _ncf_unsqueeze)            \
