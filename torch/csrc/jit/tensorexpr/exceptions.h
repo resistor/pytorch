9b1d27ec1741aa4d94913687ee54f303a0e175b7
Owen Anderson <resistor@users.noreply.github.com>
2020-03-12 14:53:49 -0700

    Make internal tensorexpr exceptions subclass from std::runtime_error. (#290)

--- torch/csrc/jit/tensorexpr/exceptions.h
+++ torch/csrc/jit/tensorexpr/exceptions.h
@@ -1,12 +1,17 @@
 #pragma once
 
+#include <stdexcept>
+
 namespace torch {
 namespace jit {
 namespace tensorexpr {
 
-class unsupported_dtype : public std::exception {
+class unsupported_dtype : public std::runtime_error {
  public:
-  unsupported_dtype() : std::exception() {}
+  unsupported_dtype()
+    : std::runtime_error("UNSUPPORTED_DTYPE") {}
+  unsupported_dtype(const std::string& err)
+    : std::runtime_error("UNSUPPORTED_DTYPE: " + err) {}
 };
 
 } // namespace tensorexpr
